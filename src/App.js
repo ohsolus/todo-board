36809bd4a7ef3e1efe3a0f15a73f3f641e4ed800
ohsolus <yesianlarenas@gmail.com>
2024-05-12 23:20:04 -0400

    Create-To-Do-Board-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Board from "./components/Board";
+import Input from "./components/input";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-12">
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h1 className="text-xl font-semibold">02- To Do Board</h1>
+        <Input taskList={taskList} setTaskList={setTaskList} />
+      </div>
+      <div className="flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12">
+        {taskList.map((task, index) => (
+          <Board
+            key={index}
+            index={index}
+            task={task}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        ))}
+      </div>
     </div>
   );
 }
